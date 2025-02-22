1504ead8ca86cf8b23b8b3e1d9443d01fee308d3
Robert Safin <robertsafin98@gmail.com>
2025-02-22 18:09:38 +0800

    4

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io"
+	"mime"
 	"net/http"
 	"os"
 	"path/filepath"
@@ -42,9 +43,10 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 	}
 	defer file.Close()
 
-	mediaType := header.Header.Get("Content-Type")
-	if mediaType == "" {
-		respondWithError(w, http.StatusBadRequest, "Missing Content-Type for thumbnail", nil)
+	mediaType, _, err := mime.ParseMediaType(header.Header.Get("Content-Type"))
+
+	if (mediaType != "image/jpeg" && mediaType != "image/png") || err != nil {
+		respondWithError(w, 400, "invalid mime type", err)
 		return
 	}
 
